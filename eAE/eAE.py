0d592a8dd56331e19ba8aa2d3e108638d6b848a3
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-03-28 18:13:24 +0100

    and now the content...

---
+++ eAE/eAE.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Provides methods for accessing the interface eAE API.
+
+"""
+import http
+import json
+
+from http import client
+
+__author__ = "Axel Oehmichen"
+__copyright__ = "Copyright 2017, Axel Oehmichen"
+__credits__ = []
+__license__ = "Apache 2"
+__version__ = "0.1"
+__maintainer__ = "Axel Oehmichen"
+__email__ = "ao1011@imperial.ac.uk"
+__status__ = "Dev"
+
+__all__ = ['eAE']
+
+
+class eAE(object):
+
+    def __init__(self, interface_ip, interface_port):
+        self.interface_ip = str(interface_ip)
+        self.interface_port = int(interface_port)
+        self.connection = http.client.HTTPSConnection(self.interface_ip, self.interface_port)
+
+    #
+    def __str__(self):
+        return "\rThe interface ip is set to: {0}\r The interface port is set to: {1}".format(self.interface_ip,
+                                                                                            self.interface_port)
+
+    def is_eae_alive(self):
+        """Retrieve the status of the eAE"""
+        self.connection.request('GET', '/interfaceEAE/utilities/isAlive')
+        res = self.connection.getresponse()
+        return int(res.read())
+
+    def retrieve_clusters(self):
+        """Retrieve the list of all available clusters"""
+        self.connection.request('GET', '/interfaceEAE/EAEManagement/retrieveClusters')
+        res = self.connection.getresponse()
+        str_response = res.read().decode('utf-8')
+        clusters = json.loads(str_response)
+        return clusters
+
+    def submit_jobs(self):
+        """Submit jobs to the eAE backend
+        
+        This method is called when a specific task needs to be deployed on a cluster.
+        """
+        #TODO: to be continued
+        self.connection.request('POST', '/interfaceEAE/OpenLava/submitJob')
+        return
+
+
+def test_methods():
+    ## Setting up the connection to interface
+    ip = "interfaceeae.doc.ic.ac.uk"
+    port = 443
+    eae = eAE(ip, port)
+
+    ## Testing if the interface is Alive
+    is_alive = eae.is_eae_alive()
+    print(is_alive)
+
+    ## We retieve the list of Clusters
+    clusters = eae.retrieve_clusters()
+    print(clusters)
+
+    ## We submit a dummy job
+    job = eae.submit_jobs()
+    print(job)
+
+if __name__ == '__main__':
+    test_methods()
+
