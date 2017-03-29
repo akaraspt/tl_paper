5179dc948175d70790741a5ef72e5250566f1ebb
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-03-29 14:36:08 +0100

    working one liner to submit to eae with demo example

--- eAE/eAE.py
+++ eAE/eAE.py
@@ -4,10 +4,13 @@
 """Provides methods for accessing the interface eAE API.
 
 """
-import http
 import json
+import stat
+import os
+import zipfile
+import httplib as http
 
-from http import client
+from subprocess import call
 from uuid import uuid4
 
 __author__ = "Axel Oehmichen"
@@ -27,12 +30,37 @@ class eAE(object):
     def __init__(self, interface_ip, interface_port):
         self.interface_ip = str(interface_ip)
         self.interface_port = int(interface_port)
-        self.connection = http.client.HTTPSConnection(self.interface_ip, self.interface_port)
+        self.connection = http.HTTPSConnection(self.interface_ip, self.interface_port)
 
-    #
     def __str__(self):
         return "\rThe interface ip is set to: {0}\r The interface port is set to: {1}".format(self.interface_ip,
                                                                                             self.interface_port)
+
+    def _create_eae_zipfile(self, zip_file_name, main_file_path, data_files=None):
+
+        to_zip = []
+        if data_files is None:
+            data_files = []
+
+        # Handle main script
+        to_zip.append(main_file_path)
+
+        # Prepare the zip file
+        zip_path = "/tmp/" + zip_file_name
+        zipf = zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_DEFLATED, allowZip64=True)
+        for f in to_zip:
+            zipf.write(f)
+        zipf.close()
+
+        # Handle other files & dirs
+        for f in data_files:
+            zipCommand = "zip -r -u -0 " + zip_path + " " + f
+            call([zipCommand], shell=True)
+
+        # Chmod 666 the zip file so it can be accessed
+        os.chmod(zip_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
+
+        return zip_path
 
     def is_eae_alive(self):
         """Retrieve the status of the eAE"""
@@ -48,37 +76,49 @@ class eAE(object):
         clusters = json.loads(str_response)
         return clusters
 
-    def submit_jobs(self, parameters_set, cluster, computation_type, main_file ,host_ip, ssh_port=22,):
+    def submit_jobs(self, parameters_set, cluster, computation_type, main_file, data_files, host_ip, ssh_port="22"):
         """Submit jobs to the eAE backend
         
         This method is called when a specific task needs to be deployed on a cluster.
         """
-        #TODO: to be continued
+
         uuid = uuid4()
-        zip_file = "{0}.zip".format(uuid)
+        zip_file_name = "{0}.zip".format(uuid)
         configs = parameters_set
-        data = dict(id=uuid, host_ip=host_ip, ssh_port=ssh_port, zip=zip_file, configs=configs, cluster=cluster,
-                    clusterType=computation_type, mainScriptExport=main_file)
-        self.connection.request('POST', '/interfaceEAE/OpenLava/submitJob', data)
-        return
+        zip_file = self._create_eae_zipfile(zip_file_name, main_file, data_files)
+        data = {'id': str(uuid), 'host_ip': host_ip, 'ssh_port': ssh_port, 'zip': zip_file, 'configs': configs,
+                'cluster': cluster, 'clusterType': computation_type, 'mainScriptExport': main_file}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/interfaceEAE/OpenLava/submitJob', data_str)
+        res = self.connection.getresponse()
+        submit_sucess = res.read()
+
+        return submit_sucess
 
 
 def test_methods():
-    ## Setting up the connection to interface
+    # Setting up the connection to interface
     ip = "interfaceeae.doc.ic.ac.uk"
     port = 443
     eae = eAE(ip, port)
 
-    ## Testing if the interface is Alive
+    # Testing if the interface is Alive
     is_alive = eae.is_eae_alive()
     print(is_alive)
 
-    ## We retieve the list of Clusters
+    # We retrieve the list of Clusters
     clusters = eae.retrieve_clusters()
     print(clusters)
 
-    ## We submit a dummy job
-    job = eae.submit_jobs()
+    # We submit a dummy job
+    parameters_set = "0\n 1\n 2\n"
+    cluster = "python_large"
+    computation_type = "Python"
+    main_file = "/PATH/TO/FILE/Demo.py"
+    data_files = ['']
+    host_ip = "X.X.X.X"
+    ssh_port = "22"
+    job = eae.submit_jobs(parameters_set, cluster, computation_type, main_file, data_files, host_ip, ssh_port)
     print(job)
 
 if __name__ == '__main__':
