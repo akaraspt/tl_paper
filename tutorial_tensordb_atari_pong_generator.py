6e8674968770c64bda47e283faedc7ddce2a8345
zsdonghao <dhsig552@163.com>
2017-05-25 13:15:20 +0100

    update DRL

--- tutorial_tensordb_atari_pong_generator.py
+++ tutorial_tensordb_atari_pong_generator.py
@@ -70,8 +70,6 @@ sampling_prob = tf.nn.softmax(probs)
 # train_op = tf.train.RMSPropOptimizer(learning_rate, decay_rate).minimize(loss)
 
 with tf.Session() as sess:
-    # init = tf.initialize_all_variables()
-    # sess.run(init)
     tl.layers.initialize_global_variables(sess)
     # if resume:
     #     load_params = tl.files.load_npz(name=model_file_name+'.npz')
